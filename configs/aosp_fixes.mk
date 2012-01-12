115166c157631f1c3ed0582b5cbae702828e76c8
fitsnugly <rickfitsnugly@gmail.com>
2012-01-12 01:10:39 -0500

    vendor: add camera effects prebuilts Pure Edits: *only install media effects on devices with no vendor partition

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -31,3 +31,10 @@ else
 PRODUCT_COPY_FILES += \
     vendor/pure/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
 endif
+
+# Camera Effects for devices without a vendor partition
+ifneq ($(filter pure_shamu,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES +=  \
+    vendor/pure/prebuilt/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/pure/prebuilt/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
+endif
