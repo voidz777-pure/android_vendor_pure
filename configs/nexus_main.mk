3f5e8634c66ea338cad8c041648813ef674a1709
beanstown106 <nbenis106@gmail.com>
2015-10-10 23:47:02 -0400

    Add SuperSu 2.52 as a prebuilt

--- configs/nexus_main.mk
+++ configs/nexus_main.mk
@@ -29,3 +29,8 @@ PRODUCT_PACKAGES += \
 # Busybox
 PRODUCT_PACKAGES += \
     Busybox
+
+# SuperSU FTW
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/supersu/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/nexus/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
--- configs/nexus_tv.mk
+++ configs/nexus_tv.mk
@@ -25,6 +25,11 @@ PRODUCT_PACKAGES += \
     Busybox \
     LockClock
 
+# SuperSU FTW
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/supersu/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/nexus/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest
