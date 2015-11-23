cdc0c706a0087ca680f1d7bfb20c706c104a265e
dhacker29 <dhackerdvm@gmail.com>
2015-11-23 03:08:03 -0500

    vendor_nexus: add android tv config, add fugu product

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,6 @@ add_lunch_combo nexus_bullhead-user
 add_lunch_combo nexus_deb-user
 add_lunch_combo nexus_flo-user
 add_lunch_combo nexus_flounder-user
+add_lunch_combo nexus_fugu-user
 add_lunch_combo nexus_hammerhead-user
 add_lunch_combo nexus_shamu-user
