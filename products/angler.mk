4d3f7171e580e76d7b0414b9b3ff2c7898b56b23
beanstown106 <nbenis106@gmail.com>
2015-12-25 14:20:05 -0500

    initial pure vendor and angler product

---
+++ products/angler.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2017 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit AOSP device configuration for angler
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := angler
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := angler
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NUF26N/3687344:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 NUF26N 3687344 release-keys"
