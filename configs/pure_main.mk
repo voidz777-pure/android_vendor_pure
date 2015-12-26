d6b45af57d9f80ff8d8e1e623799213dd67e0612
beanstown106 <nbenis106@gmail.com>
2015-12-26 04:10:53 -0500

    add aosp fixs (google props, overlays, phone apks)

--- configs/pure_main.mk
+++ configs/pure_main.mk
@@ -12,21 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Include Pure telephony configuration
-include vendor/pure/configs/pure_phone.mk
+# Include overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/pure/overlay/common
 
-# Inherit AOSP device configuration for angler
-$(call inherit-product, device/huawei/angler/aosp_angler.mk)
-
-# Override AOSP build properties
-PRODUCT_NAME := pure_angler
-PRODUCT_BRAND := google
-PRODUCT_DEVICE := angler
-PRODUCT_MODEL := Nexus 6P
-PRODUCT_MANUFACTURER := Huawei
-
-# Device Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.0/NRD90U/3155372:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.0 NRD90U 3155372 release-keys"
+# Main Required Packages
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    LiveWallpapersPicker
