129204b01da15840c0fedccfc101eb8e29b58b7c
beanstown106 <nbenis106@gmail.com>
2015-12-26 04:10:53 -0500

    add aosp fixs (google props, overlays, phone apks)

--- products/bullhead.mk
+++ products/bullhead.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Include pure telephony configuration
+include vendor/pure/configs/pure_phone.mk
+
 # Inherit AOSP device configuration for bullhead
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
