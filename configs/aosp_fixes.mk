39d91050fe2435ddd943cca36224a3f3c49a8edd
beanstown106 <nbenis106@gmail.com>
2015-12-26 04:15:07 -0500

    AOSP keyboard swype compatability *also differentiate libs based on device for arm/arm64

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -22,3 +22,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dataroaming=false
+
+# Proprietary latinime libs needed for Keyboard swyping
+ifneq ($(filter nexus_deb nexus_hammerhead nexus_flo nexus_shamu,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+endif
