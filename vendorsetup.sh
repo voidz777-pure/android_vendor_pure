b93e6a26a826fa2e44f7a38ff0939ea4c9a2d2d7
beanstown106 <nbenis106@gmail.com>
2015-12-28 12:50:01 -0500

    vendor_nexus: add tablet config, and flo, deb, flounder products

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,7 @@
 add_lunch_combo nexus_angler-user
 add_lunch_combo nexus_bullhead-user
+add_lunch_combo nexus_deb-user
+add_lunch_combo nexus_flo-user
+add_lunch_combo nexus_flounder-user
 add_lunch_combo nexus_hammerhead-user
 add_lunch_combo nexus_shamu-user
