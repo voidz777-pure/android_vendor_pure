c604800977a56dee626be5722a0d9bb52b8a036d
beanstown106 <nbenis106@gmail.com>
2016-03-08 11:35:52 -0500

    Update Build fingerprints "MMB29V"

--- products/nexus_flounder.mk
+++ products/nexus_flounder.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := htc
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=volantis \
-    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29V/2554798:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29V 2554798 release-keys"
