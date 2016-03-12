350b77e6cdf147dcb2184d8dba57f7714691c946
beanstown106 <nbenis106@gmail.com>
2016-03-12 06:03:11 -0500

    Update build fingerprints/description *angler MHC19I *bullhead MHC19J

--- products/nexus_bullhead.mk
+++ products/nexus_bullhead.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := LGE
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29V/2554798:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29V 2554798 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MHC19J/2595691:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MHC19J 2595691 release-keys"
