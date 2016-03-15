32b31460ea8115054a2a250d92f3d121e646bf90
beanstown106 <nbenis106@gmail.com>
2016-03-15 00:14:32 -0400

    Revert "drop google messenger, default is messaging"
    This reverts commit 6ac89358c443dd1105c512b054ef0bee2553222b.

--- configs/nexus_phone.mk
+++ configs/nexus_phone.mk
@@ -21,8 +21,7 @@ include vendor/nexus/configs/version.mk
 # Telephony packages
 PRODUCT_PACKAGES += \
     Stk \
-    CellBroadcastReceiver \
-    messaging
+    CellBroadcastReceiver
 
 # Allow tethering without provisioning app
 PRODUCT_PROPERTY_OVERRIDES += \
