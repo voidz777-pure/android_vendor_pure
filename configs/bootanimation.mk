YAOSP-NG/vendor_yaosp

add shamu product, and stock bootanimations

configs/bootanimation.mk
@@ -1,0 +1,27 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Add Stock Lollipop bootanimation based on device
+ifneq ($(filter nexus_angler,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nexus/prebuilt/bootanimation/angler.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nexus_bullhead,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nexus/prebuilt/bootanimation/bullhead.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nexus_shamu,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nexus/prebuilt/bootanimation/shamu.zip:system/media/bootanimation.zip
+endif

products/AndroidProducts.mk
@@ -18,3 +18,6 @@ endif
 ifeq (nexus_bullhead,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/nexus_bullhead.mk
 endif
+ifeq (nexus_shamu,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/nexus_shamu.mk
+endif

products/nexus_shamu.mk
@@ -1,0 +1,32 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include Pure Nexus telephony configuration
+include vendor/nexus/configs/nexus_phone.mk
+
+# Inherit AOSP device configuration for shamu.
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := nexus_shamu
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := shamu
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:7.0/NRD90M/3085278:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.0 NRD90M 3085278 release-keys"

vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo nexus_angler-user
 add_lunch_combo nexus_bullhead-user
+add_lunch_combo nexus_shamu-user

voidz777-pure/android_vendor_pure

configs/bootanimation.mk
@@ -1,0 +1,27 @@
+# Copyright (C) 2016 The Pure Experience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Add Stock Lollipop bootanimation based on device
+ifneq ($(filter pure_angler,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/pure/prebuilt/bootanimation/angler.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter pure_bullhead,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/pure/prebuilt/bootanimation/bullhead.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter pure_shamu,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/pure/prebuilt/bootanimation/shamu.zip:system/media/bootanimation.zip
+endif

products/AndroidProducts.mk
@@ -18,3 +18,6 @@ endif
 ifeq (pure_bullhead,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pure_bullhead.mk
 endif
+ifeq (pure_shamu,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pure_shamu.mk
+endif

products/nexus_shamu.mk
@@ -1,0 +1,32 @@
+# Copyright (C) 2016 The Pure Experience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include Pure telephony configuration
+include vendor/pure/configs/pure_phone.mk
+
+# Inherit AOSP device configuration for shamu.
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pure_shamu
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := shamu
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := motorola
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:7.0/NRD90M/3085278:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.0 NRD90M 3085278 release-keys"

vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo pure_angler-user
 add_lunch_combo pure_bullhead-user
+add_lunch_combo pure_shamu-user