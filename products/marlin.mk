aae86aecc020be0e28e3ecafa1b73dd69f022b0d
beanstown106 <nbenis106@gmail.com>
2016-12-12 15:50:52 -0500

    products: add marlin product

---
+++ products/marlin.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2017 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include pure telephony configuration
+include vendor/pure/configs/pure_phone.mk
+
+# Inherit AOSP device configuration for marlin
+$(call inherit-product, device/google/marlin/aosp_marlin.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := marlin
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := marlin
+PRODUCT_MODEL := Pixel XL
+PRODUCT_MANUFACTURER := Google
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    BUILD_FINGERPRINT=google/marlin/marlin:7.1.1/NOF27C/3687810:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 7.1.1 NOF27C 3687810 release-keys"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 add_lunch_combo angler-user
 add_lunch_combo bullhead-user
+add_lunch_combo marlin-user
 add_lunch_combo shamu-user
