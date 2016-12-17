9ebeaed9c1369c9937334517a1a936aea1ef054a
beanstown106 <nbenis106@gmail.com>
2016-12-16 23:56:48 -0500

    aosp_fixes: enable assistant on all devices

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -23,7 +23,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dataroaming=false \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
-    ro.setupwizard.rotation_locked=true
+    ro.setupwizard.rotation_locked=true \
+    ro.opa.eligible_device=true
 
 # Camera Effects for devices without a vendor partition
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
