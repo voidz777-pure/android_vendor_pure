voidz777-pure/android_vendor_custom

add tuna product

vendorsetup.sh
@@ -1,3 +1,4 @@
 add_lunch_combo angler-user
 add_lunch_combo bullhead-user
 add_lunch_combo shamu-user
+add_lunch_combo pure_tuna-userdebug

voidz777-pure/android_vendor_pure

Conflicts:
	vendorsetup.sh

vendorsetup.sh
@@ -4,3 +4,4 @@ add_lunch_combo dragon-user
 add_lunch_combo marlin-user
 add_lunch_combo sailfish-user
 add_lunch_combo shamu-user
+add_lunch_combo tuna-userdebug